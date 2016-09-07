f47b373d3ed13b4124560116f794d1a8a8de1e1c
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2016-09-07 11:45:46 -0300

    aula 7 - diretivas

--- app.js
+++ app.js
@@ -2,19 +2,7 @@
 
 var MyApp = angular.module("MyApp", []);
 
-MyApp.controller("listaController", function($scope) {
-    $scope.tarefas = [
-        { "nome" : "ir trabalhar", "prioridade" : 5 },
-        { "nome" : "ir a praia", "prioridade" : 1 },
-        { "nome" : "ir ao shopping", "prioridade" : 2 },
-    ];
-
-    $scope.adicionar = function () {
-        $scope.tarefas.push($scope.t);
-        $scope.t = null;
-    };
-
-    $scope.remover = function (i) {
-        $scope.tarefas.splice(i, 1);
-    }
+MyApp.controller("diretivasController", function($scope) {
+    $scope.developer = "Lucas dos Santos Abreu";
+    $scope.mostrar = true;
 });
